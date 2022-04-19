abc07892de9cb0aa76a1eff9e4548032d3e7cd8f
Matheus Nascimento <matheuscont2109@gmail.com>
2022-04-18 21:47:53 -0300

    Inserção de pastas do projeto e integração com banco de dados.

---
+++ crud.sql
@@ -0,0 +1,19 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION; 
+SET time_zone = "+00:00";
+
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL,
+    `nome` varchar(255) NOT NULL,
+    `telefone` varchar(13) NOT NULL,
+    `data_nascimento` date NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `sexo` varchar(15) NOT NULL,
+) ENGINE=innoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
